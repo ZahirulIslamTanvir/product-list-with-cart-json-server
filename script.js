499a1ce8a337c40c67f7129cfed8a7a3d052316d
zahirul <zahirulopel@gmail.com>
2025-08-28 11:05:04 +0600

    convert json to express server

--- script.js
+++ script.js
@@ -1,24 +1,124 @@
-// JSON Server module
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
+const express = require("express")
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+app.use(morgan("tiny"))
 
-// Make sure to use the default middleware
-const middlewares = jsonServer.defaults();
+app.use(cors())
 
-server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
-server.use(router);
-// Listen to port
-server.listen(3000, () => {
- console.log("JSON Server is running");
-});
+const jsonData ={products:[
+    {
+       image: {
+            thumbnail: "./assets/images/image-waffle-thumbnail.jpg",
+            mobile: "./assets/images/image-waffle-mobile.jpg",
+            tablet: "./assets/images/image-waffle-tablet.jpg",
+            desktop: "./assets/images/image-waffle-desktop.jpg"
+       },
+       name: "Waffle with Berries",
+       category: "Waffle",
+       price: 6.50
+    },
+    {
+        image: {
+            thumbnail: "./assets/images/image-creme-brulee-thumbnail.jpg",
+            mobile: "./assets/images/image-creme-brulee-mobile.jpg",
+            tablet: "./assets/images/image-creme-brulee-tablet.jpg",
+            desktop: "./assets/images/image-creme-brulee-desktop.jpg"
+        },
+        name: "Vanilla Bean Crème Brûlée",
+        category: "Crème Brûlée",
+        price: 7.00
+     },
+     {
+        image: {
+            thumbnail: "./assets/images/image-macaron-thumbnail.jpg",
+            mobile: "./assets/images/image-macaron-mobile.jpg",
+            tablet: "./assets/images/image-macaron-tablet.jpg",
+            desktop: "./assets/images/image-macaron-desktop.jpg"
+        },
+        name: "Macaron Mix of Five",
+        category: "Macaron",
+        price: 8.00
+     },
+     {
+        image: {
+            thumbnail: "./assets/images/image-tiramisu-thumbnail.jpg",
+            mobile: "./assets/images/image-tiramisu-mobile.jpg",
+            tablet: "./assets/images/image-tiramisu-tablet.jpg",
+            desktop: "./assets/images/image-tiramisu-desktop.jpg"
+        },
+        name: "Classic Tiramisu",
+        category: "Tiramisu",
+        price: 5.50
+     },
+     {
+        image: {
+            thumbnail: "./assets/images/image-baklava-thumbnail.jpg",
+            mobile: "./assets/images/image-baklava-mobile.jpg",
+            tablet: "./assets/images/image-baklava-tablet.jpg",
+            desktop: "./assets/images/image-baklava-desktop.jpg"
+        },
+        name: "Pistachio Baklava",
+        category: "Baklava",
+        price: 4.00
+     },
+     {
+        image: {
+            thumbnail: "./assets/images/image-meringue-thumbnail.jpg",
+            mobile: "./assets/images/image-meringue-mobile.jpg",
+            tablet: "./assets/images/image-meringue-tablet.jpg",
+            desktop: "./assets/images/image-meringue-desktop.jpg"
+        },
+        name: "Lemon Meringue Pie",
+        category: "Pie",
+        price: 5.00
+     },
+     {
+        image: {
+            thumbnail: "./assets/images/image-cake-thumbnail.jpg",
+            mobile: "./assets/images/image-cake-mobile.jpg",
+            tablet: "./assets/images/image-cake-tablet.jpg",
+            desktop: "./assets/images/image-cake-desktop.jpg"
+        },
+        name: "Red Velvet Cake",
+        category: "Cake",
+        price: 4.50
+     },
+     {
+        image: {
+            thumbnail: "./assets/images/image-brownie-thumbnail.jpg",
+            mobile: "./assets/images/image-brownie-mobile.jpg",
+            tablet: "./assets/images/image-brownie-tablet.jpg",
+            desktop: "./assets/images/image-brownie-desktop.jpg"
+        },
+        name: "Salted Caramel Brownie",
+        category: "Brownie",
+        price: 4.50
+     },
+     {
+        image: {
+            thumbnail: "./assets/images/image-panna-cotta-thumbnail.jpg",
+            mobile: "./assets/images/image-panna-cotta-mobile.jpg",
+            tablet: "./assets/images/image-panna-cotta-tablet.jpg",
+            desktop: "./assets/images/image-panna-cotta-desktop.jpg"
+        },
+        name: "Vanilla Panna Cotta",
+        category: "Panna Cotta",
+        price: 6.50
+     }
+] 
+}
 
-// Export the Server API
-module.exports = server;
+app.get("/",(req,res)=>{    
+    return res.status(200).json(jsonData)
+})
+
+
+app.get("/products",(req,res)=>{    
+    return res.status(200).json(jsonData.products)
+})
+
+app.listen(3000,()=>{
+    console.log("App is running at port 3000");
+    
+})
